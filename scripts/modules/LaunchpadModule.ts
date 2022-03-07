928cd52d4085b78717b953ca7671135763a47a64
Egor Gavrilov <modagavr@gmail.com>
2022-03-07 16:28:51 +0400

    FEAT: Launchpad Deploy Script + Verification

--- scripts/modules/LaunchpadModule.ts
+++ scripts/modules/LaunchpadModule.ts
@@ -1,4 +1,4 @@
-import { ethers, upgrades } from 'hardhat'
+import { ethers, run, upgrades } from 'hardhat'
 
 import { LaunchpadModule } from '../../typechain-types'
 
@@ -10,6 +10,25 @@ async function main() {
   await launchpad.deployed()
 
   console.log('LaunchpadModule:', launchpad.address)
+
+  await (
+    await launchpad.setCoreAddresses(
+      '0x72cc6E4DE47f673062c41C67505188144a0a3D84',
+      '0xCA49EcF7e7bb9bBc9D1d295384663F6BA5c0e366',
+      '0xB42DD79C056d4b511c07c29d4c35403b47bE29B9'
+    )
+  ).wait()
+
+  console.log('LaunchpadModule: SetCoreAddresses Success')
+
+  try {
+    await run('verify:verify', {
+      address: '',
+      contract: 'contracts/modules/LaunchpadModule.sol:LaunchpadModule'
+    })
+  } catch {
+    console.log('Verification problem (LaunchpadModule)')
+  }
 }
 
 main()
