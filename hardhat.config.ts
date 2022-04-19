3bb8b916020edc5966af2d2a1feb32465a31307f
Egor Gavrilov <modagavr@gmail.com>
2022-04-19 15:59:02 +0800

    FEAT: XinFin Deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -127,6 +127,12 @@ const config: HardhatUserConfig = {
       accounts: [(process.env.PRIVATE_KEY as string) || vanityKey]
     },
 
+    xinfin: {
+      url: 'https://rpc.xinfin.network',
+      chainId: 50,
+      accounts: [(process.env.PRIVATE_KEY as string) || vanityKey]
+    },
+
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
       chainId: 4,
